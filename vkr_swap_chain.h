6ce1d5d52babb935efc6f4856c9550947bea3202
Mathieu Chartier <mathieu.a.chartier@gmail.com>
2022-03-01 19:15:29 -0500

    Update swap chain code

--- vkr_swap_chain.h
+++ vkr_swap_chain.h
@@ -41,69 +41,9 @@ public:
 
   size_t ImageCount() const { return swap_images_.size(); }
 
-  
-  void InitSync() {
-    VkSemaphoreCreateInfo sem_info{};
-    sem_info.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
-    for (size_t i = 0; i < ImageCount(); ++i) {
-      VkSemaphore sem{};
-      vkCreateSemaphore(engine_->Device(), &sem_info, engine_->Callbacks(), &sem);
-      img_available_.push_back(sem);
-      vkCreateSemaphore(engine_->Device(), &sem_info, engine_->Callbacks(), &sem);
-      render_finished_.push_back(sem);
-      VkFenceCreateInfo fence_info;
-      fence_info.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
-      fence_info.flags = VK_FENCE_CREATE_SIGNALED_BIT;
-      VkFence fence{};
-      vkCreateFence(engine_->Device(), &fence_info, engine_->Callbacks(), &fence);
-      render_fence_.push_back(fence);
-    }
-  }
-
-  uint32_t BeginFrame() {
-    auto res = vkWaitForFences(engine_->Device(), 1, &render_fence_[current_frame_], VK_TRUE, UINT64_MAX);
-    uint32_t image_idx{};
-    VkResult res = vkAcquireNextImageKHR(engine_->Device(), swap_chain_, UINT64_MAX, img_available_[current_frame_], VK_NULL_HANDLE, &image_idx);
-    if (res == VK_ERROR_OUT_OF_DATE_KHR || res == VK_SUBOPTIMAL_KHR) {
-      // Recreate swap chain??
-    } else if (res != VK_SUCCESS) {
-      std::cerr << "Error with acquiring next image" << std::endl;
-    }
-    return image_idx;
-
-  }
-
-  void EndFrame(uint32_t image_idx) {
-    VkSubmitInfo submit_info{};
-    submit_info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-    VkSemaphore wait_sem[] = {img_available_[current_frame_]};
-    VkPipelineStageFlags wait_stages[] = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
-    submit_info.waitSemaphoreCount = 1;
-    submit_info.pWaitSemaphores = wait_sem;
-    submit_info.pWaitDstStageMask = wait_stages;
-    submit_info.commandBufferCount = 1;
-    submit_info.pCommandBuffers = nullptr; // &command_buffer[image_idx_];
-    VkSemaphore signal_sems[] = {render_finished_[current_frame_]};
-    submit_info.signalSemaphoreCount = 1;
-    submit_info.pSignalSemaphores = signal_sems;  
-    vkResetFences(engine_->Device(), 1, &render_fence_[current_frame_]);
-    if (vkQueueSubmit(gfx_queue, 1, &submit_info, render_fence_[current_frame_]) != VK_SUCCESS) {
-      throw std::runtime_error("failed to submit draw command buffer!");
-    }
-
-    VkPresentInfoKHR presentInfo{};
-    presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
-    presentInfo.waitSemaphoreCount = 1;
-    presentInfo.pWaitSemaphores = signal_sems;
-    VkSwapchainKHR swap_chains[] = {swap_chain_};
-    presentInfo.swapchainCount = 1;
-    presentInfo.pSwapchains = swap_chains;
-    presentInfo.pImageIndices = &image_idx;
-    presentInfo.pResults = nullptr; // Optional
-    vkQueuePresentKHR(present_queue, &presentInfo);
-
-    current_frame_ = (current_frame_ + 1) % img_available_.size();
-  }
+  void InitSync();
+  uint32_t BeginFrame();
+  void EndFrame(VkQueue gfx_queue, VkQueue present_queue, uint32_t image_idx);
 
   template <typename Visitor>
   std::optional<VkSurfaceFormatKHR> ChooseFormat(const Visitor& visitor) {
